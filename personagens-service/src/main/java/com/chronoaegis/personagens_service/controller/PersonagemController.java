bb1d5879410387e3cd68554f07e52f81c1457eb3
LaraCruz18 <laravsc@hotmail.com>
2026-05-22 19:56:47 -0300

    feat: criando controller

--- personagens-service/src/main/java/com/chronoaegis/personagens_service/controller/PersonagemController.java
+++ personagens-service/src/main/java/com/chronoaegis/personagens_service/controller/PersonagemController.java
@@ -1,4 +1,31 @@
 package com.chronoaegis.personagens_service.controller;
 
+import com.chronoaegis.personagens_service.dto.PersonagemDTO;
+import com.chronoaegis.personagens_service.model.Personagem;
+import com.chronoaegis.personagens_service.service.PersonagemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/personagens")
 public class PersonagemController {
+
+    private final PersonagemService service;
+
+    public PersonagemController(PersonagemService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Personagem> criar(@RequestBody PersonagemDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.criarPersonagem(dto));
+    }
+
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<Personagem>> buscarPorUsuario(@PathVariable Long usuarioId) {
+        return ResponseEntity.ok(service.buscarFichas(usuarioId));
+    }
 }
