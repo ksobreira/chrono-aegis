49083aea971954714e5319648eda2ed0d184245d
LaraCruz18 <laravsc@hotmail.com>
2026-05-22 19:47:25 -0300

    feat: DTO e Repository

--- personagens-service/src/main/java/com/chronoaegis/personagens_service/dto/PersonagemDTO.java
+++ personagens-service/src/main/java/com/chronoaegis/personagens_service/dto/PersonagemDTO.java
@@ -1,4 +1,14 @@
 package com.chronoaegis.personagens_service.dto;
 
 public class PersonagemDTO {
+    private Long usuarioId;
+    private String nome;
+    private String classe;
+
+    public Long getUsuarioId() { return usuarioId; }
+    public void setUsuarioId(Long usuarioId) { this.usuarioId = usuarioId; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getClasse() { return classe; }
+    public void setClasse(String classe) { this.classe = classe; }
 }
