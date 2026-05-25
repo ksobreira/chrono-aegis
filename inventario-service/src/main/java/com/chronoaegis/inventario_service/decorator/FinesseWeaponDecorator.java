14f707fee6e9b7f2ae522fdd9ef2073e6d7da216
kdev_sobreira <kauam.morais09@gmail.com>
2026-05-25 19:02:40 -0300

    "feat: implementação do padrão Decorator para tags de armas"

--- inventario-service/src/main/java/com/chronoaegis/inventario_service/decorator/FinesseWeaponDecorator.java
+++ inventario-service/src/main/java/com/chronoaegis/inventario_service/decorator/FinesseWeaponDecorator.java
@@ -1,4 +1,21 @@
 package com.chronoaegis.inventario_service.decorator;
 
-public class FinesseWeaponDecorator {
+public class FinesseWeaponDecorator extends WeaponDecorator {
+
+    private int agilidade;
+
+    public FinesseWeaponDecorator(WeaponComponent weapon, int agilidade) {
+        super(weapon);
+        this.agilidade = agilidade;
+    }
+
+    @Override
+    public int getDano(){
+        return Math.max(super.getDano(), agilidade);
+    }
+
+    @Override
+    public String getNome(){
+        return super.getNome() + "Acuidade";
+    }
 }
