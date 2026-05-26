e2a3234929f170b12867c1b702b6eb3b6d09f2e8
henrigarciadev <henriluizgarcia@gmail.com>
2026-05-26 16:30:02 -0300

    feat: implementar padrao Command para acoes do jogador

---
+++ combate-service/src/main/java/com/chronoaegis/combate_service/command/AtacarCommand.java
@@ -0,0 +1,38 @@
+package com.chronoaegis.combate_service.command;
+
+import com.chronoaegis.combate_service.dto.ResultadoTurnoDTO;
+import com.chronoaegis.combate_service.model.BatalhaSession;
+import com.chronoaegis.combate_service.strategy.DamageCalculationStrategy;
+import com.chronoaegis.combate_service.strategy.FinesseStrategy;
+import com.chronoaegis.combate_service.strategy.NormalDamageStrategy;
+
+public class AtacarCommand implements ComandoBatalha {
+
+    @Override
+    public ResultadoTurnoDTO executar(BatalhaSession sessao) {
+        DamageCalculationStrategy strategy;
+        if ("ACUIDADE".equalsIgnoreCase(sessao.getTagArma())) {
+            strategy = new FinesseStrategy();
+        } else if ("VANGUARDA".equalsIgnoreCase(sessao.getPosicao())) {
+            strategy = new NormalDamageStrategy(1.2);
+        } else {
+            strategy = new NormalDamageStrategy(0.8);
+        }
+
+        int dano = strategy.calcular(sessao.getAtaquePersonagem(), sessao.getDanoArma(), sessao.getMonstro().getDefesa());
+
+        sessao.getMonstro().setHpAtual(sessao.getMonstro().getHpAtual() - dano);
+
+        sessao.getLog().add("Jogador atacou! Dano: " + dano + " | HP Monstro: " + sessao.getMonstro().getHpAtual());
+
+        ResultadoTurnoDTO resultado = new ResultadoTurnoDTO();
+        resultado.setHpJogador(sessao.getHpAtualPersonagem());
+        resultado.setHpMonstro(sessao.getMonstro().getHpAtual());
+        resultado.setTurnoAtual("TURNO_INIMIGO");
+        resultado.setLog(sessao.getLog());
+        resultado.setFimDeBatalha(false);
+        resultado.setVitoria(false);
+
+        return resultado;
+    }
+}
